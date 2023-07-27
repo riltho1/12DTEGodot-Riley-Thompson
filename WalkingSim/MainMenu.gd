e7dbc5f81a114dea3b5ca84b71312463162694cf
riltho1 <100237088+riltho1@users.noreply.github.com>
2023-07-27 20:45:27 +1200

    Completed my title menu, I'm going to do my enemy pathfinding next.

--- WalkingSim/MainMenu.gd
+++ WalkingSim/MainMenu.gd
@@ -9,3 +9,22 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_option_pressed():
+	$AnimationPlayer.play("MainToOptions")
+
+func _on_back_pressed():
+	$AnimationPlayer.play("OptionsToMain")
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://world.tscn")
+
+func _on_check_box_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
