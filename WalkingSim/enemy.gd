ec8239408da7a0f7a5c4fb9f6e28fb16c96074a6
riltho1 <100237088+riltho1@users.noreply.github.com>
2023-08-02 10:00:21 +1200

    Made it so the enemy turns to face me, I just need to make the walking animation play and do a jumpscare

--- WalkingSim/enemy.gd
+++ WalkingSim/enemy.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+var enemy_walking = true
+
 @onready var nav_agent = $NavigationAgent3D
 @onready var player = get_node("/root/World/Player")
 
@@ -10,7 +12,8 @@ func _ready():
 
 func get_target():
 	nav_agent.target_position = player.global_transform.origin
-	movement_speed *= 1.05
+	movement_speed *= 1.1
+	print(movement_speed)
 
 func _physics_process(delta):
 	if nav_agent.is_navigation_finished():
@@ -20,6 +23,11 @@ func _physics_process(delta):
 	var next_position = nav_agent.get_next_path_position()
 	var new_velocity = next_position - current_position
 	new_velocity = new_velocity.normalized() * movement_speed
-	
+	look_at(player.global_transform.origin)
 	velocity = new_velocity
 	move_and_slide()
+
+func enemy_movement():
+	if enemy_walking == true:
+		enemy_walking = false
+		$AnimationPlayer.play("Walking")
