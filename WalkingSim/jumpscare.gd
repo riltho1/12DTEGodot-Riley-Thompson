4375e9d56baca63f14f7668a34d5d7f55dd77d04
riltho1 <100237088+riltho1@users.noreply.github.com>
2023-08-07 13:25:52 +1200

    Made a defeat screen for after the jumpscare. I need to add a win screen and change my pickup to a switch.

---
+++ WalkingSim/jumpscare.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://defeat_screen.tscn")
